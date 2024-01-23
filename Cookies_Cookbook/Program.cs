c70872ba5280e3ea2a9766b2296c0727e3fe5fdc
LorenzoNicco <lorenzoniccolai91@gmail.com>
2024-01-23 12:28:59 +0100

    Aggiunta logica di base dell'app

--- Cookies_Cookbook/Program.cs
+++ Cookies_Cookbook/Program.cs
@@ -1,5 +1,92 @@
-﻿Console.WriteLine("Create a new cookie recipe! Available ingredients are:");
+﻿//Console.WriteLine("Create a new cookie recipe! Available ingredients are:");
+
+using System.Reflection.Metadata;
+
+var cookieApp = new CookieApp();
+cookieApp.Run();
+
 Console.ReadKey();
+
+//CLASSE GENERALE DELL'APP
+public class CookieApp
+{
+    //Area di salvataggio delle ricette
+    private readonly RecipiesDb _recipiesDb;
+    //Interazione con l'utente
+    private readonly UserInteractionWithRecipies _userInteractionWithRecipies;
+
+    //Costruttore
+    public CookieApp(RecipiesDb recipiesDb, UserInteractionWithRecipies userInteractionWithRecipies)
+    {
+        _recipiesDb = recipiesDb;
+        _userInteractionWithRecipies = userInteractionWithRecipies;
+    }
+
+    //METODO CON LOGICA DI FUNZIONAMENTO DELL'APP
+    public void Run()
+    {
+        //Legge il contenuto del file dove sono salvate le ricette
+        var recipiesList = _recipiesDb.Read(filePath);
+        //Stampa le ricette esistenti
+        _userInteractionWithRecipies.PrintExistingRecipies(recipiesList);
+        //Chiede all'utente di creare una nuova ricetta
+        _userInteractionWithRecipies.AskToMakeRecipe();
+        //Prende gli ingredienti scelti dall'utente
+        var ingredients = _userInteractionWithRecipies.ReadIngredientsFromUser();
+
+        if(ingredients.Count > 0)
+        {
+            //Prende gli ingredienti scelti dall'utente e li salva nel file
+            var recipies = new Recipie(ingredients);
+            recipiesList.Add(recipie);
+            _recipiesDb.Write(filePath, recipieList);
+
+            //Avvisa l'utente che la ricetta è stata salvata nel file
+            _userInteractionWithRecipies.ShowMessage("Recipe added:");
+            _userInteractionWithRecipies.ShowMessage(recipe.ToString());
+        }
+        else
+        {
+            _userInteractionWithRecipies.ShowMessage("No ingredients have been selected. Recipe will not be saved.")
+        }
+
+        //Chiusura dell'app
+        _userInteractionWithRecipies.Exit();
+    }
+}
+
+//CLASSE PER L'INTERAZIONE CON L'UTENTE
+public class UserInteractionWithRecipies
+{
+}
+
+//CLASSE PER IL SALVATAGGIO DELLE RICETTE
+public class RecipiesDb
+{
+}
+
+public class IngredientsPrinter
+{
+    readonly Ingredients[] ingredients = new Ingredients[]
+    {
+        new WheatFlour(),
+        new CoconoutFlour(),
+        new Butter(),
+        new Chocolate(),
+        new Sugar(),
+        new Cardamom(),
+        new Cinnamon(),
+        new CocoaPowder(),
+    };
+
+    public void Printer()
+    {
+        foreach (Ingredients ingredient in ingredients)
+        {
+            Console.WriteLine($"{ingredient.ID}. {ingredient.Name}");
+        }
+    }
+}
 
 public abstract class Ingredients
 {
